ca59bb7f4a7fdcc9ba97b1f1f31366614fe2ca11
off-dngw <79832647+off-dngw@users.noreply.github.com>
2022-07-04 02:55:06 +0900

    done

--- alecture/client.tsx
+++ alecture/client.tsx
@@ -1,15 +1,7 @@
-import 'core-js/stable';
-import 'regenerator-runtime/runtime';
 import React from 'react';
 import { render } from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
-import axios from 'axios';
-
-import App from './layouts/App';
-
-axios.defaults.withCredentials = true;
-axios.defaults.baseURL =
-  process.env.NODE_ENV === 'production' ? 'https://sleact.nodebird.com' : 'http://localhost:3090';
+import App from '@layouts/App';
 
 render(
   <BrowserRouter>
@@ -17,3 +9,7 @@ render(
   </BrowserRouter>,
   document.querySelector('#app'),
 );
+
+// pages - 서비스 페이지
+// components - 짜잘 컴포넌트
+// layouts - 공통 레이아웃
